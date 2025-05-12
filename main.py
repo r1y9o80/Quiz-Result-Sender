65030a7f4d91c3bcd09670292bbcde23875c97fe
r1y9o80 <Ryo-official@outlook.jp>
2025-05-12 11:35:39 +0900

    update

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import requests
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=["POST"])
+def getQuiz_fetch():
+    # get_json()を使う
+    data = request.get_json()  # 修正箇所
+    message = data.get('message', 'No message')
+    send_QuizResult(message)
+    return 'OK'  # 追加して、Flaskが応答を返すようにします
+
+def send_QuizResult(textContent):
+    load_dotenv()
+    WEB_HOOK_URL = os.getenv("WEB_HOOK")
+    data = {
+        "content": textContent,
+        "username": "Quiz-Result-Sender"
+    }
+    requests.post(WEB_HOOK_URL, json=data)
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
+
+
