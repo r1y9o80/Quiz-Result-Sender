f07d2f8aa4cf926d6321e817791b87466d35a1d4
r1y9o80 <Ryo-official@outlook.jp>
2025-06-14 00:43:10 +0900

    update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from flask import Flask, request
 from flask_cors import CORS
 
+
+WEB_KEYS = {"QuizResult": "WEB_HOOK_URL_QUIZRESULT", "Message": "WEB_HOOK_URL_MESSAGE"}
 app = Flask(__name__)
 CORS(app)
 
@@ -11,16 +13,17 @@ CORS(app)
 def getQuiz_fetch():
     try:
         data = request.get_json()  # 修正: request.json() -> request.get_json()
-        message = data.get('message', 'No message')  # 修正: get_json() は不要
-        send_QuizResult(message)
+        header = data.get('header')
+        message = data.get('body')  # 修正: get_json() は不要
+        send_QuizResult(header, message)
         return "The data has been perfectly sent.",200
     except Exception as e:
         print(f"Error: {e}")
         return f"Error: {e}", 500
 
-def send_QuizResult(textContent):
+def send_QuizResult(header, textContent):
     load_dotenv()
-    WEB_HOOK_URL = os.getenv("WEB_HOOK_URL")
+    WEB_HOOK_URL = os.getenv(WEB_KEYS[header])
     if WEB_HOOK_URL:  # WEB_HOOK_URL が設定されているか確認
         data = {
             "content": textContent,
